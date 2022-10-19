1cef386d58d57ce29f790d824c01cb1334e5ef5c
Andrius Romakso <andrius.rom@gmail.com>
2022-10-19 18:06:18 +0200

    [WIP, BUG FIX] Battle system.

--- Card Game/Assets/Project/Scripts/BatleSystem.cs
+++ Card Game/Assets/Project/Scripts/BatleSystem.cs
@@ -9,8 +9,8 @@ public class BatleSystem : NetworkBehaviour
     public static Action<GameObject, CardData> onGetOponentData; // called when player release button on oponent card entered with mouse
     public static Action onStartBattle;
 
-    CardData playerCardData;
-    CardData oponentCardData;
+    [SerializeField] CardData playerCardData;
+    [SerializeField] CardData oponentCardData;
 
     GameObject playerCardObject;
     GameObject oponentCardObject;
@@ -26,34 +26,36 @@ public class BatleSystem : NetworkBehaviour
         oponentCardData = data;
     }
 
-    public void StartBatlle()
+    public void StartBattle()
     {
         var oponenPlacedCard = oponentCardObject.GetComponent<PlacedCard>();
         var playerPlacedCard = playerCardObject.GetComponent<PlacedCard>();
 
         var attackerResult = oponenPlacedCard.cardHealth - playerCardData.power;
+        //Debug.Log($"oponenPlacedCard.cardHealth {oponenPlacedCard.cardHealth} -- playerCardData.power {playerCardData.power}-- oponenPlacedCard.cardHealth - playerCardData.power {oponenPlacedCard.cardHealth - playerCardData.power}");
         var defenderResponseResult = playerPlacedCard.cardHealth - oponentCardData.power;
+        //Debug.Log($"playerPlacedCard.cardHealth {playerPlacedCard.cardHealth} -- oponentCardData.power {oponentCardData.power}-- playerPlacedCard.cardHealth - oponentCardData.power {playerPlacedCard.cardHealth - oponentCardData.power}");
 
         Debug.Log($"Start batlle! attacker res {attackerResult} -- defender res {defenderResponseResult} :20:red;".Interpolate());
         if (attackerResult <= 0)
         {
             Destroy(oponentCardObject);
             RPC_DestroyPlayerCard(playerCardData.cardID);
-            playerPlacedCard.cardHealth -= defenderResponseResult;
+            playerPlacedCard.cardHealth -= oponentCardData.power;
         }
         else
         {
-            oponenPlacedCard.cardHealth -= attackerResult;
+            playerPlacedCard.cardHealth -= oponentCardData.power;
         }
         if (defenderResponseResult <= 0)
         {
             Destroy(playerCardObject);
             RPC_DestroyOponentCard(oponentCardData.cardID);
-            oponenPlacedCard.cardHealth -= defenderResponseResult;
+            oponenPlacedCard.cardHealth -= playerCardData.power;
         }
         else
         {
-            playerPlacedCard.cardHealth -= defenderResponseResult;
+            oponenPlacedCard.cardHealth -= playerCardData.power;
         }
     }
     [Rpc(InvokeLocal = false)]
@@ -86,14 +88,14 @@ public class BatleSystem : NetworkBehaviour
     void OnEnable()
     {
         onGetPlayerData += SetPlayerData;
-        onGetPlayerData += SetOponentData;
-        onStartBattle += StartBatlle;
+        onGetOponentData += SetOponentData;
+        onStartBattle += StartBattle;
     }
     void OnDisable()
     {
         onGetPlayerData -= SetPlayerData;
-        onGetPlayerData -= SetOponentData;
-        onStartBattle -= StartBatlle;
+        onGetOponentData -= SetOponentData;
+        onStartBattle -= StartBattle;
 
     }
 }
