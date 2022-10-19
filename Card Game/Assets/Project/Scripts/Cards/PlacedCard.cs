f84a027e1302d1ce4ee3d99f96d18859041e1822
Andrius Romakso <andrius.rom@gmail.com>
2022-10-19 12:00:40 +0200

    UI-Extensions: UILinerenderer implemented.

--- Card Game/Assets/Project/Scripts/Cards/PlacedCard.cs
+++ Card Game/Assets/Project/Scripts/Cards/PlacedCard.cs
@@ -1,15 +1,22 @@
 using Fusion;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI.Extensions;
 
-public class PlacedCard : NetworkBehaviour, ISpawned
+[RequireComponent(typeof(UILineRendererList))]
+public class PlacedCard : NetworkBehaviour, ISpawned, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     public string ID;
     public CardData cardData;
-
+    UILineRendererList lineRenderer;
     [HideInInspector] public CardSpawner spawner;
+    [SerializeField] Vector2 offset; // -850, -370
+    Vector2 startingPosition;
+    Vector2 lastDragPosition;
 
     void Start()
     {
+        lineRenderer = GetComponent<UILineRendererList>();
         try
         {
             if (FusionCallbacks.runner.IsServer)
@@ -37,4 +44,37 @@ public class PlacedCard : NetworkBehaviour, ISpawned
         cardData = spawner.cardsDatabase.GetCardbyId(id);
         spawner.InitializePlacedCard(this, cardData);
     }
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log($"Pointer down");
+        var rect = GetComponent<RectTransform>();
+        startingPosition = new Vector3(rect.position.x, rect.position.y);
+        if (lineRenderer == null)
+        {
+            Debug.LogError($"Line renderer list null");
+            return;
+        }
+        lineRenderer.AddPoint(startingPosition + offset);
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 pos = new Vector3(eventData.position.x, eventData.position.y);
+
+        if (lastDragPosition != pos)
+        {
+            lineRenderer.RemovePoint(lastDragPosition);
+        }
+        lineRenderer.AddPoint(pos + offset);
+        lastDragPosition = pos + offset;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log($"Pointer up. Initiate batle");
+        lineRenderer.RemovePoint(startingPosition);
+        lineRenderer.RemovePoint(lastDragPosition);
+
+    }
+
 }
